26c2ba64f56e2fc46869f480aec24d4500ab78a7
skiriel <gabrieldllopes@gmail.com>
2023-03-20 21:38:38 -0300

    Add project files.

---
+++ Binary_Search/Binary_Search/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro");
+            int x = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ Binary_Search/Binary_Search/Search.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Binary_Search
+{
+    static class Search
+    {
+        static int MaxValue = 0;
+        static public int BinSearch(int num)
+        {
+            
+        }
+    }
+}
